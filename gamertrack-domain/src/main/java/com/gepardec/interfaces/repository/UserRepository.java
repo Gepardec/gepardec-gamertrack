469b09f4d508d4ebe7f48c43fe2bc6f8e8bf46a3
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-11-25 10:11:35 +0100

    add interface for user repo & service

--- gamertrack-domain/src/main/java/com/gepardec/interfaces/repository/UserRepository.java
+++ gamertrack-domain/src/main/java/com/gepardec/interfaces/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.gepardec.interfaces.repository;
 
+import com.gepardec.model.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+    void saveUser(User user);
+    void updateUser(User user);
+    void deleteUser(User user);
+    Optional<User> getUsers();
+    User getUserById(long id);
 }
--- gamertrack-domain/src/main/java/com/gepardec/interfaces/services/UserService.java
+++ gamertrack-domain/src/main/java/com/gepardec/interfaces/services/UserService.java
@@ -1,4 +1,14 @@
 package com.gepardec.interfaces.services;
 
+import com.gepardec.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    void saveUser(User user);
+    void updateUser(User user);
+    void deleteUser(User user);
+    Optional<User> getUsers();
+    User getUserById(long id);
 }
