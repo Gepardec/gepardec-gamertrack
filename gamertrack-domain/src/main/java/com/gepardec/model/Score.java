6bc185f57ed0a8b61f41d4b0bf34ce3cdea55646
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-11-25 09:58:12 +0100

    add User and Empty-Score classes

---
+++ gamertrack-domain/src/main/java/com/gepardec/model/Score.java
@@ -0,0 +1,7 @@
+package com.gepardec.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Score extends AbstractEntity{
+}
--- gamertrack-domain/src/main/java/com/gepardec/model/User.java
+++ gamertrack-domain/src/main/java/com/gepardec/model/User.java
@@ -1,4 +1,46 @@
 package com.gepardec.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends AbstractEntity{
+
+    @NotEmpty(message = "Firstname must be set")
+    public String firstname;
+    @NotEmpty(message = "Lastname must be set")
+    public String lastname;
+
+    public List<Score> scores;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @OneToMany
+    public List<Score> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Score> scores) {
+        this.scores = scores;
+    }
 }
