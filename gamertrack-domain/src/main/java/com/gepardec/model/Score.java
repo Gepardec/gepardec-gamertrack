f009c12f0ac095ddabab331bde6a09734d36f512
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-11-28 09:25:04 +0100

    add bi-directional-relation between user and score

--- gamertrack-domain/src/main/java/com/gepardec/model/Score.java
+++ gamertrack-domain/src/main/java/com/gepardec/model/Score.java
@@ -1,7 +1,24 @@
 package com.gepardec.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Score extends AbstractEntity{
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Game game;
+    public double score;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
 }
