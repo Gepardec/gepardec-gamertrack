8e817a4bbdfe7d0ea9846cb59b9610ac6b06d0cd
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-11-29 12:51:27 +0100

    add UserServiceImplTest

---
+++ gamertrack-service/src/test/java/com/gepradec/impl/UserServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.gepradec.impl;
+
+import com.gepardec.adapters.output.persistence.repository.GameRepositoryImpl;
+import com.gepardec.adapters.output.persistence.repository.UserRepositoryImpl;
+import com.gepardec.impl.GameServiceImpl;
+import com.gepardec.impl.UserServiceImpl;
+import com.gepardec.model.Game;
+import com.gepardec.model.User;
+import jakarta.persistence.EntityManager;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@EnableAutoWeld
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Mock
+    EntityManager entityManager;
+
+    @Mock
+    UserRepositoryImpl userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Test
+    void ensureSaveAndReadGameWorks() {
+        User user = new User("Max","Muster");
+
+        when(userRepository.saveUser(user)).thenReturn(Optional.of(user));
+        assertEquals(userService.saveUser(user).get().getFirstname(), user.getFirstname());
+    }
+}
