68d8a8e195b8953ce4047f57e30f599dd2ffa54f
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-12-02 14:29:26 +0100

    add logging for user Repo and Service

--- gamertrack-db/src/main/java/com/gepardec/adapters/output/persistence/repository/UserRepositoryImpl.java
+++ gamertrack-db/src/main/java/com/gepardec/adapters/output/persistence/repository/UserRepositoryImpl.java
@@ -3,8 +3,12 @@ package com.gepardec.adapters.output.persistence.repository;
 import com.gepardec.interfaces.repository.UserRepository;
 import com.gepardec.model.User;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
@@ -12,46 +16,48 @@ import java.util.Optional;
 @ApplicationScoped
 public class UserRepositoryImpl implements UserRepository, Serializable {
 
-  @PersistenceContext()
-  protected EntityManager entityManager;
-
-  @Override
-  public Optional<User> saveUser(User user) {
-    entityManager.persist(user);
-    User userSaved = entityManager.find(User.class, user.getId());
-
-    return Optional.ofNullable(userSaved);
-  }
+    private static final Logger log = LoggerFactory.getLogger(UserRepositoryImpl.class);
+    @PersistenceContext()
+    protected EntityManager entityManager;
 
 
-  public Optional<User> updateUser(Long id, User user) {
-    entityManager.merge(user);
-    User userMerged = entityManager.find(User.class, user.getId());
+    @Override
+    public List<User> findAllUsersIncludeDeleted() {
+        List<User> resultList = entityManager.createQuery("SELECT u FROM User u", User.class)
+                .getResultList();
+        log.info("Find all users including deleted user. Returned list of size:{}", resultList.size());
 
-    return Optional.ofNullable(userMerged);
-  }
+        return resultList;
+    }
+    @Override
+    public List<User> findAllUsers() {
+        List<User> resultList = entityManager.createQuery("SELECT u FROM User u Where u.firstname <> 'DELETED' And u.lastname <> 'U$ER' ", User.class)
+                .getResultList();
+        log.info("Find all users. Returned list of size:{}", resultList.size());
+        return resultList;
 
-  @Override
-  public void deleteUser(User user) {
-    entityManager.remove(user);
-  }
+    }
 
-  @Override
-  public List<User> findAllUsers() {
-    return entityManager.createQuery("SELECT u FROM User u", User.class)
-        .getResultList();
-  }
+    @Override
+    public Optional<User> findUserById(long id) {
+        List<User> resultList = entityManager.createQuery("SELECT u FROM User u WHERE u.id = :id", User.class)
+                .setParameter("id", id)
+                .getResultList();
+        log.info("Find user with id: {}. Returned list of size:{}", id, resultList.size());
+        return resultList.isEmpty() ? Optional.empty() : Optional.of(resultList.getFirst());
+    }
+    @Override
+    public Optional<User> saveUser(User user) {
+        entityManager.persist(user);
+        User userSaved = entityManager.find(User.class, user.getId());
+        log.info("Saved user with id: {}", userSaved.getId());
+        return  Optional.ofNullable(userSaved);
+    }
 
-  @Override
-  public Optional<User> findUserById(long id) {
-    return Optional.ofNullable(
-        entityManager.createQuery("SELECT u FROM User u WHERE u.id = :id", User.class)
-            .setParameter("id", id)
-            .getResultList().getFirst());
-  }
 
-  @Override
-  public Optional<User> findUserReferencesById(Long userId) {
-    return Optional.of(entityManager.getReference(User.class, userId));
-  }
+    @Override
+    public void deleteUser(User user) {
+        log.info("Deleted user with id: {}", user.getId());
+        entityManager.remove(user);
+    }
 }
