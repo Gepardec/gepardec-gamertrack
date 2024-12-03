1631c61435bd34fbfc7e927aee23c8c5636be305
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-12-03 15:51:42 +0100

    add description and apiResponses for UserResource

--- gamertrack-application/src/main/java/com/gepardec/rest/api/UserResource.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/api/UserResource.java
@@ -4,6 +4,9 @@ import com.gepardec.model.User;
 import com.gepardec.rest.model.command.CreateUserCommand;
 import com.gepardec.rest.model.command.UpdateGameCommand;
 import com.gepardec.rest.model.command.UpdateUserCommand;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -18,26 +21,53 @@ public interface UserResource {
 
     public static final String BASE_USER_PATH = "users";
 
-    @GET
-    public Response getUsers();
-    @Path("/includeDeleted")
-    @GET
-    public Response getUsersIncludeDeleted();
-
-    @Path("{id}")
-    @GET
-    public Response getUser(@PathParam("id") Long id);
-
+    @Operation(summary = "Post a user", description = "Returns the created user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not Created - The user was not created")
+    })
     @POST
     Response createUser(@Valid CreateUserCommand userCommand);
 
+    @Operation(summary = "Updated user by id", description = "Returns the updated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not Found - The user was not be found")
+    })
     @Path("{id}")
     @PUT
     public Response updateUser(@PathParam("id") Long id, @Valid UpdateUserCommand updateUserCommand);
 
+    @Operation(summary = "Delete User by id", description = "Returns the deleted user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not Found - The user was not be found")
+    })
     @Path("{id}")
     @DELETE
     public Response deleteUser(@PathParam("id") Long id);
+
+    @Operation(summary = "Get all users", description = "Returns a list of users")
+    @ApiResponses(@ApiResponse(responseCode = "200", description = "Successfully retrieved"))
+    @GET
+    public Response getUsers();
+
+    @Operation(summary = "Get all users including the deleted User", description = "Returns a list of users including")
+    @ApiResponses(@ApiResponse(responseCode = "200", description = "Successfully retrieved"))
+    @Path("/includeDeleted")
+    @GET
+    public Response getUsersIncludeDeleted();
+
+    @Operation(summary = "Get User by id", description = "Returns user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Not Found - The user was not be found")
+    })
+    @Path("{id}")
+    @GET
+    public Response getUser(@PathParam("id") Long id);
+
+
 
 
 }
