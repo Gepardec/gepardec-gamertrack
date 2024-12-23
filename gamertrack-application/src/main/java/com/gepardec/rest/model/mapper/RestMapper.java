55922bf5f588536ea0a5f85d61f065276e3b1e93
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-12-23 11:13:03 +0100

    WIP: refactor RestMapper to mapping Model-classes issue #27

--- gamertrack-application/src/main/java/com/gepardec/rest/model/mapper/RestMapper.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/mapper/RestMapper.java
@@ -4,7 +4,6 @@ import com.gepardec.model.Score;
 import com.gepardec.model.User;
 import com.gepardec.model.Game;
 import com.gepardec.model.Match;
-import com.gepardec.model.dto.ScoreDto;
 import com.gepardec.rest.model.command.CreateGameCommand;
 import com.gepardec.rest.model.command.CreateMatchCommand;
 import com.gepardec.rest.model.command.CreateScoreCommand;
@@ -17,37 +16,34 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class RestMapper {
 
-  public Score toScoreDto(CreateScoreCommand scoreCommand) {
-
-    return new ScoreDto(scoreCommand.userId(), scoreCommand.gameId());
+  public Score createScoreCommandtoScore(CreateScoreCommand scoreCommand) {
+    return new Score(null,scoreCommand.user(), scoreCommand.game(), scoreCommand.scorePoints());
   }
 
-  public User CreateUserCommandtoUser(CreateUserCommand createUserCommand) {
-    return new User(0, createUserCommand.firstname(), createUserCommand.lastname(), false);
+  public User createUserCommandtoUser(CreateUserCommand createUserCommand) {
+    return new User(null, createUserCommand.firstname(), createUserCommand.lastname(), false);
   }
 
-  public User UpdateUserCommandtoUser(Long id, UpdateUserCommand updateUserCommand) {
+  public User updateUserCommandtoUserDto(Long id, UpdateUserCommand updateUserCommand) {
     return new User(id, updateUserCommand.firstname(), updateUserCommand.lastname(),
         updateUserCommand.deactivated());
   }
 
 
-  public Match toMatchDto(Long id, UpdateMatchCommand gameOutcomeCommand) {
-    return new Match(id, gameOutcomeCommand.gameId(),
-        gameOutcomeCommand.userIds());
+  public Match updateMatchCommandtoMatch(Long id, UpdateMatchCommand gameOutcomeCommand) {
+    return new Match(id, gameOutcomeCommand.game(),
+        gameOutcomeCommand.users());
   }
 
-  public Match toMatchDto(CreateMatchCommand createGameCommand) {
-    return new Match(null, createGameCommand.gameId(),
-        createGameCommand.userIds());
+  public Match createMatchCommandtoMatch(CreateMatchCommand createGameCommand) {
+    return new Match(null, createGameCommand.game(), createGameCommand.users());
   }
 
-  public Game toGameDto(Long id, UpdateGameCommand updateGameCommand) {
-    return new Game(id, updateGameCommand.title(),
-        updateGameCommand.rules());
+  public Game updateGameCommandtoGameDto(Long id, UpdateGameCommand updateGameCommand) {
+    return new Game(id, updateGameCommand.title(), updateGameCommand.rules());
   }
 
-  public Game toGameDto(CreateGameCommand createGameCommand) {
-    return new Game(0L, createGameCommand.title(), createGameCommand.rules());
+  public Game createGameCommandtoGameDto(CreateGameCommand createGameCommand) {
+    return new Game(null, createGameCommand.title(), createGameCommand.rules());
   }
 }
