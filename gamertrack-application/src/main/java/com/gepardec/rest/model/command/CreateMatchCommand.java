61ce7a92731c91e6e7a4bae8aa59273d410fa717
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2024-12-23 11:07:20 +0100

    WIP: refactor Commands to use Entities instead of ids issue #27

--- gamertrack-application/src/main/java/com/gepardec/rest/model/command/CreateMatchCommand.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/command/CreateMatchCommand.java
@@ -1,7 +1,10 @@
 package com.gepardec.rest.model.command;
 
+import com.gepardec.model.Game;
+import com.gepardec.model.User;
+
 import java.util.List;
 
-public record CreateMatchCommand(Long gameId, List<Long> userIds) {
+public record CreateMatchCommand(Game game, List<User> users) {
 
 }
--- gamertrack-application/src/main/java/com/gepardec/rest/model/command/CreateScoreCommand.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/command/CreateScoreCommand.java
@@ -1,8 +1,10 @@
 package com.gepardec.rest.model.command;
 
+import com.gepardec.model.Game;
+import com.gepardec.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 //Only temporary for testing
 
-public record CreateScoreCommand (@NotNull long userId, @NotNull long gameId, @NotNull double scorePoints) {
+public record CreateScoreCommand (@NotNull User user, @NotNull Game game, @NotNull double scorePoints) {
 }
