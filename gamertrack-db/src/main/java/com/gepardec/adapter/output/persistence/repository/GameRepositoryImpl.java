074e8f2cfe5c68e01553454da95a561951c8d07f
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-01-03 10:48:15 +0100

    add logger to GameRepositoryImpl & GameServiceImpl issue: #30

--- gamertrack-db/src/main/java/com/gepardec/adapter/output/persistence/repository/GameRepositoryImpl.java
+++ gamertrack-db/src/main/java/com/gepardec/adapter/output/persistence/repository/GameRepositoryImpl.java
@@ -12,10 +12,14 @@ import jakarta.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 @Transactional
 public class GameRepositoryImpl implements GameRepository, Serializable {
+
+  Logger logger = LoggerFactory.getLogger(GameRepositoryImpl.class);
 
   @Inject
   private EntityManager em;
@@ -25,42 +29,55 @@ public class GameRepositoryImpl implements GameRepository, Serializable {
 
   @Override
   public Optional<Game> saveGame(Game game) {
+    logger.info("Saving game %s".formatted(game));
     GameEntity gameEntity = gameMapper.gameModelToGameEntity(game);
-
     em.persist(gameEntity);
     em.flush();
-    GameEntity foundGameEntity = em.find(GameEntity.class, gameEntity.getId());
-    return Optional.ofNullable(gameMapper.gameEntityToGameModel(foundGameEntity));
+    Optional<GameEntity> foundGameEntity = Optional.ofNullable(
+        em.find(GameEntity.class, gameEntity.getId()));
+
+    foundGameEntity.ifPresentOrElse(fge -> logger.info("Saved game %s".formatted(fge)),
+        () -> logger.error("Could not save game %s".formatted(game)));
+
+    return Optional.ofNullable(gameMapper.gameEntityToGameModel(foundGameEntity.get()));
   }
 
   @Override
   public void deleteGame(Long gameId) {
+    logger.info("Deleting game with ID: %s".formatted(gameId));
     em.remove(em.find(GameEntity.class, gameId));
   }
 
   @Override
   public Optional<Game> updateGame(Game game) {
-    GameEntity gameEntityOld = em.find(GameEntity.class, game.getId());
+    Optional<GameEntity> gameEntityOld = Optional.ofNullable(
+        em.find(GameEntity.class, game.getId()));
 
-    return gameEntityOld != null
-        ? Optional.of(this.gameMapper.gameEntityToGameModel(
-        em.merge(this.gameMapper.gameModelToExitstingGameEntity(game, gameEntityOld))))
-        : Optional.empty();
+    gameEntityOld.ifPresentOrElse(geo -> logger.info("Updating game %s".formatted(geo)),
+        () -> logger.info("Could not update game %s, because no entity with id %s".formatted(game,
+            game.getId())));
+
+    return gameEntityOld.map(gameEntity -> this.gameMapper.gameEntityToGameModel(
+        em.merge(this.gameMapper.gameModelToExitstingGameEntity(game, gameEntity))));
   }
 
   @Override
   public Optional<Game> findGameByToken(String token) {
+    logger.info("Finding game by token: %s".formatted(token));
+
     var found = em.createQuery(
             "select g from GameEntity g where g.token = :token",
             GameEntity.class)
         .setParameter("token", token)
         .getResultList().stream().findFirst();
 
+    logger.info("Found game: %s".formatted(found));
     return found.map(gameEntity -> gameMapper.gameEntityToGameModel(gameEntity));
   }
 
   @Override
   public List<Game> findAllGames() {
+    logger.info("Finding all games");
     return em.createQuery("select g from GameEntity g", GameEntity.class)
         .getResultList()
         .stream()
--- gamertrack-domain/src/main/java/com/gepardec/impl/service/GameServiceImpl.java
+++ gamertrack-domain/src/main/java/com/gepardec/impl/service/GameServiceImpl.java
@@ -10,10 +10,15 @@ import jakarta.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Stateless
 @Transactional
 public class GameServiceImpl implements GameService, Serializable {
+
+  private final Logger logger = LoggerFactory.getLogger(GameServiceImpl.class);
+
 
   @Inject
   private GameRepository gameRepository;
@@ -22,6 +27,8 @@ public class GameServiceImpl implements GameService, Serializable {
 
   @Override
   public Optional<Game> saveGame(Game game) {
+    logger.info("Saving game: %s".formatted(game));
+
     if (gameRepository.gameExistsByGameName(game.getName())) {
       return Optional.empty();
     }
@@ -31,6 +38,7 @@ public class GameServiceImpl implements GameService, Serializable {
 
   @Override
   public Optional<Game> deleteGame(String token) {
+    logger.info("Deleting game with token %s".formatted(token));
     Optional<Game> game = gameRepository.findGameByToken(token);
     if (game.isEmpty()) {
       return Optional.empty();
@@ -46,27 +54,33 @@ public class GameServiceImpl implements GameService, Serializable {
     Optional<Game> newGame = Optional.empty();
 
     if (game == null) {
+      logger.error("Game is null");
       return Optional.empty();
     }
 
+    logger.info("Updating game with token %s".formatted(game.getToken()));
     Optional<Game> gameOld = findGameByToken(game.getToken());
 
     if (gameOld.isPresent()) {
       game.setId(gameOld.get().getId());
+      logger.info("Updating game with new values %s".formatted(game));
       newGame = gameRepository.updateGame(game);
     }
-    
+
+    logger.error("Game with token %s could not be found".formatted(game.getToken()));
     return newGame;
 
   }
 
   @Override
   public Optional<Game> findGameByToken(String token) {
+    logger.info("Finding game with token %s".formatted(token));
     return gameRepository.findGameByToken(token);
   }
 
   @Override
   public List<Game> findAllGames() {
+    logger.info("Finding all games");
     return gameRepository.findAllGames();
   }
 }
