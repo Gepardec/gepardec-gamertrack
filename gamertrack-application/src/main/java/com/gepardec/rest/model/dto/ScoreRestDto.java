44499b65abd581464a0352288e835806c9d1de88
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-01-07 08:59:34 +0100

    rename attribute and add Validation annotation to User & Score-RestDto issue #27

--- gamertrack-application/src/main/java/com/gepardec/rest/model/dto/ScoreRestDto.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/dto/ScoreRestDto.java
@@ -1,8 +1,9 @@
 package com.gepardec.rest.model.dto;
 
 import com.gepardec.model.Score;
+import jakarta.validation.constraints.NotNull;
 
-public record ScoreRestDto(Long id, UserRestDto userRestDto, GameRestDto gameRestDto, double score) {
+public record ScoreRestDto(@NotNull Long id, @NotNull UserRestDto user, @NotNull GameRestDto game, double score) {
     public ScoreRestDto(Score score){
         this(score.getId(), new UserRestDto(score.getUser()), new GameRestDto(score.getGame()), score.getScorePoints());
     }
--- gamertrack-application/src/main/java/com/gepardec/rest/model/dto/UserRestDto.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/dto/UserRestDto.java
@@ -2,8 +2,10 @@ package com.gepardec.rest.model.dto;
 
 
 import com.gepardec.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record UserRestDto(Long id, String firstname, String lastname, boolean deactivated ) {
+public record UserRestDto(@NotNull Long id, @NotBlank String firstname, @NotBlank String lastname, boolean deactivated ) {
     public UserRestDto(User user){
         this(user.getId(),user.getFirstname(),user.getLastname(),user.isDeactivated());
     }
