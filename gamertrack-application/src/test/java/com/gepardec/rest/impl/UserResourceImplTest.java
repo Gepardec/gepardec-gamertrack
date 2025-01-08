deae06259da059bf87f98ec4d54d3ecfd914f1b5
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-01-08 11:10:30 +0100

    add RestIntegrationTests for User issue #34

--- gamertrack-application/src/test/java/com/gepardec/rest/impl/UserResourceImplTest.java
+++ gamertrack-application/src/test/java/com/gepardec/rest/impl/UserResourceImplTest.java
@@ -1,61 +1,145 @@
 package com.gepardec.rest.impl;
 
-import com.gepardec.RestConfig;
-import com.gepardec.core.services.UserService;
-import com.gepardec.model.User;
-import com.gepardec.rest.api.UserResource;
 import com.gepardec.rest.model.command.CreateUserCommand;
-import jakarta.inject.Inject;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
-import org.jboss.arquillian.junit5.ArquillianExtension;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+import com.gepardec.rest.model.command.UpdateUserCommand;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.net.URL;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.Matchers.equalTo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@ExtendWith(ArquillianExtension.class)
 public class UserResourceImplTest {
 
-        @ArquillianResource
-        private URL deploymentURL;
-
-        @Inject
-        UserResourceImpl userResource;
-
-        @Inject
-        UserService userService;
-
-        @Deployment(testable = false)
-        public static WebArchive create()
-        {
-            return ShrinkWrap.create(WebArchive.class)
-                    .addPackage(User.class.getPackage())
-                    .addClasses(UserResource.class, UserResourceImpl.class, RestConfig.class);
+        @BeforeAll
+        public static void setup() {
+                RestAssured.baseURI = "http://localhost:8080/gepardec-gamertrack/api/v1";
         }
 
         @Test
-        void createUser(@ArquillianResteasyResource UserResource userResource)
-        {
+        public void ensureCreateUserWorks() {
+                with().when()
+                        .contentType("application/json")
+                        .body(new CreateUserCommand("max","Muster"))
+                        .request("POST", "/users")
+                        .then()
+                        .statusCode(201)
+                        .assertThat()
+                        .body("firstname", equalTo("max"))
+                        .log().body();
+        }
 
-            //        Given
-            CreateUserCommand createUserCommand = new CreateUserCommand("John", "Doe");
+        @Test
+        public void ensureUpdateUserWorks() {
+                String token = with()
+                        .when()
+                        .contentType("application/json")
+                        .body(new CreateUserCommand("Jakob", "Muster"))
+                        .request("POST", "/users")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("token");
 
-            //        When
-            final User result = userResource.createUser(createUserCommand).readEntity(User.class);
+                with()
+                        .when()
+                        .contentType("application/json")
+                        .body(new UpdateUserCommand("Paul", "Mustermann", false))
+                        .pathParam("token", token)
+                        .request("PUT", "/users/{token}")
+                        .then()
+                        .statusCode(200)
+                        .log().body();
 
-            //        Then
-            assertNotNull(result);
-            assertNotNull(result.getId());
-            assertNotNull(result.getToken());
-            assertEquals(createUserCommand.firstname(), result.getFirstname());
-            assertEquals(createUserCommand.lastname(), result.getLastname());
+                with()
+                        .when()
+                        .contentType("application/json")
+                        .pathParam("token", token)
+                        .request("GET", "/users/{token}")
+                        .then()
+                        .statusCode(200)
+                        .assertThat()
+                        .body(
+                                "firstname", equalTo("Paul"),
+                                "lastname", equalTo("Mustermann")
+                        )
+                        .log().body();
+        }
+
+        @Test
+        public void ensureDeleteUserWorks() {
+                String token = with()
+                        .when()
+                        .contentType("application/json")
+                        .body(new CreateUserCommand("Max", "Muster"))
+                        .request("POST", "/users")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("token");
+
+                with()
+                        .when()
+                        .contentType("application/json")
+                        .pathParam("token", token)
+                        .request("DELETE", "/users/{token}")
+                        .then()
+                        .statusCode(200)
+                        .log().body();
+        }
+
+        @Test
+        public void ensureDeleteNotExistingUserReturnsNotFound() {
+                String token = with()
+                        .when()
+                        .contentType("application/json")
+                        .body(new CreateUserCommand("Max", "Muster"))
+                        .request("POST", "/users")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("token");
+
+                with()
+                        .when()
+                        .contentType("application/json")
+                        .request("DELETE", "/users/sdfk23s3df36sa")
+                        .then()
+                        .statusCode(404)
+                        .log().body();
+        }
+        @Test
+        public void ensureGetUserReturnsOk() {
+                with().when()
+                        .request("GET", "/users")
+                        .then()
+                        .statusCode(200);
+        }
+
+        @Test
+        public void ensureGetUserByTokenReturnsOk() {
+                String token = with()
+                        .when()
+                        .contentType("application/json")
+                        .body(new CreateUserCommand("Max", "Muster"))
+                        .request("POST", "/users")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("token");
+
+                with()
+                        .when()
+                        .contentType("application/json")
+                        .pathParam("token", token)
+                        .request("GET", "/users/{token}")
+                        .then()
+                        .statusCode(200)
+                        .body(
+                                "firstname", equalTo("Max"),
+                                "lastname", equalTo("Muster")
+                        )
+                        .log().body();
         }
 
 }
