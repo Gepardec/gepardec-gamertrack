95f2a922d90521b7e85c0caa1409ef6e46a0c5d0
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-01-16 11:06:13 +0100

    fix small issues after rebase
    issue #34

--- gamertrack-application/src/main/java/com/gepardec/rest/model/dto/MatchRestDto.java
+++ gamertrack-application/src/main/java/com/gepardec/rest/model/dto/MatchRestDto.java
@@ -3,6 +3,7 @@ package com.gepardec.rest.model.dto;
 import com.gepardec.model.Match;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -10,37 +11,37 @@ import java.util.Objects;
 public record MatchRestDto(@NotBlank String token, @NotNull GameRestDto gameRestDto,
                            @NotNull List<UserRestDto> userRestDtos) {
 
-  public MatchRestDto(Match match) {
-    this(match.getToken(),
-        new GameRestDto(match.getGame()),
-        new ArrayList<>(
-            match.getUsers().stream()
-                .map(UserRestDto::new)
-                .toList()
-        ));
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+    public MatchRestDto(Match match) {
+        this(match.getToken(),
+                new GameRestDto(match.getGame()),
+                new ArrayList<>(
+                        match.getUsers().stream()
+                                .map(UserRestDto::new)
+                                .toList()
+                ));
     }
-    MatchRestDto that = (MatchRestDto) o;
-    return Objects.equals(game, that.game) && Objects.equals(token, that.token)
-        && Objects.equals(users, that.users);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(token, game, users);
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MatchRestDto that = (MatchRestDto) o;
+        return Objects.equals(gameRestDto, that.gameRestDto) && Objects.equals(token, that.token)
+                && Objects.equals(userRestDtos, that.userRestDtos);
+    }
 
-  @Override
-  public String toString() {
-    return "MatchRestDto{" +
-        "token='" + token + '\'' +
-        ", game=" + game +
-        ", users=" + users +
-        '}';
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(token, gameRestDto, userRestDtos);
+    }
+
+    @Override
+    public String toString() {
+        return "MatchRestDto{" +
+                "token='" + token + '\'' +
+                ", game=" + gameRestDto +
+                ", users=" + userRestDtos +
+                '}';
+    }
 }
