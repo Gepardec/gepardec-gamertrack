f44fb896c472511a6a73e670482c172059fe7448
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-01-16 10:19:00 +0100

    fix small issues after rebase
    issue #30

--- gamertrack-db/src/test/java/com/gepardec/adapter/output/persistence/repository/mapper/GameMapperTest.java
+++ gamertrack-db/src/test/java/com/gepardec/adapter/output/persistence/repository/mapper/GameMapperTest.java
@@ -1,10 +1,5 @@
 package com.gepardec.adapter.output.persistence.repository.mapper;
 
-import static com.gepardec.TestFixtures.game;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import com.gepardec.TestFixtures;
 import com.gepardec.adapter.output.persistence.entity.GameEntity;
 import com.gepardec.model.Game;
 import jakarta.persistence.EntityManager;
@@ -14,43 +9,47 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.gepardec.TestFixtures.game;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(MockitoExtension.class)
 public class GameMapperTest {
 
-  @Mock
-  EntityManager entityManager;
+    @Mock
+    EntityManager entityManager;
 
-  @InjectMocks
-  GameMapper entityMapper = new GameMapper();
+    @InjectMocks
+    GameMapper entityMapper = new GameMapper();
 
 
-  @Test
-  void ensureGameModelToGameWithReferenceEntityMappingWorks() {
-    Game game = TestFixtures.gameToGameDto(game());
+    @Test
+    void ensureGameModelToGameWithReferenceEntityMappingWorks() {
+        Game game = game();
 
-    GameEntity mappedGame = entityMapper.gameModelToGameEntity(game);
+        GameEntity mappedGame = entityMapper.gameModelToGameEntity(game);
 
-    assertDoesNotThrow(() -> NullPointerException.class);
-    assertEquals(game.getToken(), mappedGame.getToken());
-    assertEquals(game.getName(), mappedGame.getName());
-    assertEquals(game.getRules(), mappedGame.getRules());
-  }
+        assertDoesNotThrow(() -> NullPointerException.class);
+        assertEquals(game.getToken(), mappedGame.getToken());
+        assertEquals(game.getName(), mappedGame.getName());
+        assertEquals(game.getRules(), mappedGame.getRules());
+    }
 
-  @Test
-  void ensureGameModelToGameWithReferenceMappingWorksProvidingModelAndEntity() {
-    Game game = TestFixtures.gameToGameDto(game());
+    @Test
+    void ensureGameModelToGameWithReferenceMappingWorksProvidingModelAndEntity() {
+        Game game = game();
 
-    GameEntity existingGameEntity = new GameEntity(game.getId(), game.getToken(), game.getName(),
-        game.getRules());
+        GameEntity existingGameEntity = new GameEntity(game.getId(), game.getToken(), game.getName(),
+                game.getRules());
 
-    GameEntity mappedGame = entityMapper.gameModelToExitstingGameEntity(game, existingGameEntity);
+        GameEntity mappedGame = entityMapper.gameModelToExitstingGameEntity(game, existingGameEntity);
 
-    assertDoesNotThrow(() -> NullPointerException.class);
-    assertEquals(game.getToken(), mappedGame.getToken());
-    assertEquals(game.getId(), mappedGame.getId());
-    assertEquals(game.getName(), mappedGame.getName());
-    assertEquals(game.getRules(), mappedGame.getRules());
-  }
+        assertDoesNotThrow(() -> NullPointerException.class);
+        assertEquals(game.getToken(), mappedGame.getToken());
+        assertEquals(game.getId(), mappedGame.getId());
+        assertEquals(game.getName(), mappedGame.getName());
+        assertEquals(game.getRules(), mappedGame.getRules());
+    }
 
 }
 
--- gamertrack-domain/src/main/java/com/gepardec/model/Match.java
+++ gamertrack-domain/src/main/java/com/gepardec/model/Match.java
@@ -1,65 +1,68 @@
 package com.gepardec.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public class Match {
 
-  private Long id;
-  private String token;
-  @NotNull(message = "Game must not be null")
-  private Game game;
-  @NotEmpty(message = "User List must not be null or Empty")
-  private List<User> users;
+    private Long id;
+    private String token;
+    @NotNull(message = "Game must not be null")
+    private Game game;
+    @NotEmpty(message = "User List must not be null or Empty")
+    private List<User> users;
 
-  public Match() {
-  }
+    public Match() {
+    }
 
-  public Match(Long id, String token, Game game, List<User> users) {
-    this.id = id;
-    this.token = token;
-    this.game = game;
-    this.users = users;
-  }
+    public Match(Long id, String token, Game game, List<User> users) {
+        this.id = id;
+        this.token = token;
+        this.game = game;
+        this.users = users;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public Game getGame() {
-    return game;
-  }
+    public Game getGame() {
+        return game;
+    }
 
-  public void setGame(Game game) {
-    this.game = game;
-  }
+    public void setGame(Game game) {
+        this.game = game;
+    }
 
-  public List<User> getUsers() {
-    return users;
-  }
+    public List<User> getUsers() {
+        return users;
+    }
 
-  public void setUsers(List<User> users) {
-    this.users = users;
-  }
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
-  public String getToken() {
-    return token;
-  }
+    public String getToken() {
+        return token;
+    }
 
-  public void setToken(String token) {
-    this.token = token;
-  }
+    public void setToken(String token) {
+        this.token = token;
+    }
 
-  @Override
-  public String toString() {
-    return "Match{" +
-        "id=" + id +
-        ", key='" + token + '\'' +
-        ", game=" + game +
-        ", users=" + users +
-        '}';
-  }
+    @Override
+    public String toString() {
+        return "Match{" +
+                "id=" + id +
+                ", key='" + token + '\'' +
+                ", game=" + game +
+                ", users=" + users +
+                '}';
+    }
 }
