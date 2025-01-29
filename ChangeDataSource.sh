4727236f87664e532da57b52e395d2b275d6b405
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-01-29 08:34:40 +0100

    remove unnecessary shutdown issue #44

--- ChangeDataSource.sh
+++ ChangeDataSource.sh
@@ -12,5 +12,4 @@ echo "=> WildFly started. Now configuring datasource."
 
 /opt/jboss/wildfly/bin/jboss-cli.sh --connect <<EOF
 /subsystem=datasources/data-source=ExampleDS:write-attribute(name=connection-url, value="jdbc:h2:file:/opt/jboss/wildfly/gamertrackDB;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=\${wildfly.h2.compatibility.mode:REGULAR}")
-shutdown
 EOF
