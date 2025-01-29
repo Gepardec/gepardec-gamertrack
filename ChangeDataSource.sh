008da959713f6525e47600b01fbc801c38edd0e7
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-01-29 08:29:13 +0100

    remove unnecessary reload in ChangeDataSource issue #44

--- ChangeDataSource.sh
+++ ChangeDataSource.sh
@@ -2,7 +2,6 @@
 
 /opt/jboss/wildfly/bin/standalone.sh &
 
-# Warte, bis WildFly vollständig hochgefahren ist und erreichbar ist
 echo "=> Waiting for WildFly to start"
 until curl -s http://localhost:8080 > /dev/null; do
     echo "Waiting for WildFly..."
@@ -11,13 +10,7 @@ done
 
 echo "=> WildFly started. Now configuring datasource."
 
-# Jetzt den CLI-Befehl ausführen, um die Datenquelle zu konfigurieren
 /opt/jboss/wildfly/bin/jboss-cli.sh --connect <<EOF
 /subsystem=datasources/data-source=ExampleDS:write-attribute(name=connection-url, value="jdbc:h2:file:/opt/jboss/wildfly/gamertrackDB;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=\${wildfly.h2.compatibility.mode:REGULAR}")
-reload
 shutdown
 EOF
-
-
-
-# Halte das Skript am Laufen, damit der Container nicht beendet wird
