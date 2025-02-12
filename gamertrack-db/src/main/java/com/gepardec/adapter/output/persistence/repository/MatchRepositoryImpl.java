3ee11c9cd6ba82d41210d66c95c30db057691d96
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-02-12 08:57:21 +0100

    add pagination to matchendpoint/repository
    known-issue: headers not set, has a conflict with xtotalcountfilter
    
    fixes #51

--- gamertrack-db/src/main/java/com/gepardec/adapter/output/persistence/repository/MatchRepositoryImpl.java
+++ gamertrack-db/src/main/java/com/gepardec/adapter/output/persistence/repository/MatchRepositoryImpl.java
@@ -1,134 +1,173 @@
 package com.gepardec.adapter.output.persistence.repository;
 
 import com.gepardec.adapter.output.persistence.entity.MatchEntity;
+import com.gepardec.adapter.output.persistence.entity.UserEntity;
 import com.gepardec.adapter.output.persistence.repository.mapper.MatchMapper;
 import com.gepardec.core.repository.MatchRepository;
 import com.gepardec.model.Match;
+import jakarta.data.page.PageRequest;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 @Transactional
 public class MatchRepositoryImpl implements MatchRepository {
 
-  private final Logger logger = LoggerFactory.getLogger(MatchRepositoryImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(MatchRepositoryImpl.class);
 
-  @Inject
-  private EntityManager em;
+    @Inject
+    private EntityManager em;
 
-  @Inject
-  MatchMapper matchMapper;
+    @Inject
+    MatchMapper matchMapper;
 
-  @Override
-  public Optional<Match> saveMatch(Match match) {
-    MatchEntity matchToSave = matchMapper.matchModelToMatchEntityWithReference(match);
-    logger.info("Saving  match {}", match);
+    @Override
+    public Optional<Match> saveMatch(Match match) {
+        MatchEntity matchToSave = matchMapper.matchModelToMatchEntityWithReference(match);
+        logger.info("Saving  match {}", match);
 
-    em.persist(matchToSave);
-    em.flush();
+        em.persist(matchToSave);
+        em.flush();
 
-    return findMatchById(matchToSave.getId());
-  }
-
-  @Override
-  public List<Match> findAllMatches() {
-    logger.info("Finding all matches");
-    return em.createQuery("select go from MatchEntity go", MatchEntity.class)
-        .getResultList()
-        .stream()
-        .map(matchMapper::matchEntityToMatchModel)
-        .toList();
-  }
-
-  @Override
-  public Optional<Match> findMatchById(Long id) {
-    logger.info("Finding match by id: %s".formatted(id));
-
-    return Optional.ofNullable(em.find(MatchEntity.class, id))
-        .map(matchMapper::matchEntityToMatchModel);
-  }
-
-  @Override
-  public Optional<Match> findMatchByToken(String token) {
-    logger.info("Finding match by token: %s".formatted(token));
-
-    var found = em.createQuery(
-            "select m from MatchEntity m where m.token = :token",
-            MatchEntity.class)
-        .setParameter("token", token)
-        .getResultList().stream().findFirst();
-
-    return found.map(matchEntity -> matchMapper.matchEntityToMatchModel(matchEntity));
-  }
-
-  @Override
-
-  public void deleteMatch(Long matchId) {
-    Optional<MatchEntity> matchToDelete = Optional.ofNullable(em.find(MatchEntity.class, matchId));
-
-    matchToDelete.ifPresentOrElse(
-        mtd -> logger.info("Deleting match with id: %s".formatted(mtd.getId())),
-        () -> logger.info("Could not find match with ID %s".formatted(matchId)));
-
-    em.remove(matchToDelete.get());
-  }
-
-  @Override
-  public Optional<Match> updateMatch(Match matchNew) {
-    logger.info("updating match with id: %s".formatted(matchNew.getId()));
-
-    MatchEntity match = em.find(MatchEntity.class, matchNew.getId());
-    if (match == null) {
-      return Optional.empty();
+        return findMatchById(matchToSave.getId());
     }
 
-    MatchEntity updatedMatch = em.merge(
-        matchMapper.matchModelToMatchEntityWithReference(matchNew, match));
+    @Override
+    public List<Match> findAllMatches() {
+        logger.info("Finding all matches");
+        return em.createQuery("select go from MatchEntity go", MatchEntity.class)
+                .getResultList()
+                .stream()
+                .map(matchMapper::matchEntityToMatchModel)
+                .toList();
+    }
 
-    return Optional.of(matchMapper.matchEntityToMatchModel(updatedMatch));
-  }
+    @Override
+    public Optional<Match> findMatchById(Long id) {
+        logger.info("Finding match by id: %s".formatted(id));
 
-  @Override
-  public List<Match> findMatchesByUserToken(String userToken) {
-    logger.info("Finding all matches by user token: %s".formatted(userToken));
-    var query = em.createQuery(
-        "select m from MatchEntity m inner join m.users u where u.token = :userToken ",
-        MatchEntity.class);
+        return Optional.ofNullable(em.find(MatchEntity.class, id))
+                .map(matchMapper::matchEntityToMatchModel);
+    }
 
-    query.setParameter("userToken", userToken);
-    return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
-  }
+    @Override
+    public Optional<Match> findMatchByToken(String token) {
+        logger.info("Finding match by token: %s".formatted(token));
 
-  @Override
-  public List<Match> findMatchesByGameToken(String gameToken) {
-    logger.info("Finding all games outcomes by game token: %s".formatted(gameToken));
-    var query = em.createQuery("select m from MatchEntity m where m.game.token = :gameToken ",
-        MatchEntity.class);
+        var found = em.createQuery(
+                        "select m from MatchEntity m where m.token = :token",
+                        MatchEntity.class)
+                .setParameter("token", token)
+                .getResultList().stream().findFirst();
 
-    query.setParameter("gameToken", gameToken);
-    return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
-  }
+        return found.map(matchEntity -> matchMapper.matchEntityToMatchModel(matchEntity));
+    }
 
-  @Override
-  public List<Match> findMatchesByGameTokenAndUserToken(String gameToken, String userToken) {
-    logger.info(
-        "Finding matches by UserToken: %s and GameToken: %s".formatted(userToken, gameToken));
-    var query = em.createQuery(
-        "select m from MatchEntity m inner join m.users u where (:userToken is NULL OR u.token = :userToken) and (:gameToken is NULL OR m.game.token = :gameToken)",
-        MatchEntity.class);
-    query.setParameter("userToken", userToken);
-    query.setParameter("gameToken", gameToken);
-    return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
-  }
+    @Override
 
-  @Override
-  public Boolean existsMatchById(Long matchId) {
-    return findMatchById(matchId).isPresent();
-  }
+    public void deleteMatch(Long matchId) {
+        Optional<MatchEntity> matchToDelete = Optional.ofNullable(em.find(MatchEntity.class, matchId));
+
+        matchToDelete.ifPresentOrElse(
+                mtd -> logger.info("Deleting match with id: %s".formatted(mtd.getId())),
+                () -> logger.info("Could not find match with ID %s".formatted(matchId)));
+
+        em.remove(matchToDelete.get());
+    }
+
+    @Override
+    public Optional<Match> updateMatch(Match matchNew) {
+        logger.info("updating match with id: %s".formatted(matchNew.getId()));
+
+        MatchEntity match = em.find(MatchEntity.class, matchNew.getId());
+        if (match == null) {
+            return Optional.empty();
+        }
+
+        MatchEntity updatedMatch = em.merge(
+                matchMapper.matchModelToMatchEntityWithReference(matchNew, match));
+
+        return Optional.of(matchMapper.matchEntityToMatchModel(updatedMatch));
+    }
+
+    @Override
+    public List<Match> findMatchesByUserToken(String userToken) {
+        logger.info("Finding all matches by user token: %s".formatted(userToken));
+        var query = em.createQuery(
+                "select m from MatchEntity m inner join m.users u where u.token = :userToken ",
+                MatchEntity.class);
+
+        query.setParameter("userToken", userToken);
+        return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
+    }
+
+    @Override
+    public List<Match> findMatchesByGameToken(String gameToken, PageRequest pageRequest) {
+        logger.info("Finding all games outcomes by game token: %s".formatted(gameToken));
+
+        int pageIndex = (int) pageRequest.page() - 1;
+        int offset = Math.max(0, pageIndex * pageRequest.size());
+
+        var query = em.createQuery("select m from MatchEntity m where m.game.token = :gameToken ",
+                MatchEntity.class);
+
+        query.setParameter("gameToken", gameToken);
+        query.setFirstResult(offset);
+        query.setMaxResults(pageRequest.size());
+        return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
+    }
+
+    @Override
+    public List<Match> findMatchesByGameTokenAndUserToken(String gameToken, String userToken) {
+        logger.info(
+                "Finding matches by UserToken: %s and GameToken: %s".formatted(userToken, gameToken));
+        var query = em.createQuery(
+                "select m from MatchEntity m inner join m.users u where (:userToken is NULL OR u.token = :userToken) and (:gameToken is NULL OR m.game.token = :gameToken)",
+                MatchEntity.class);
+        query.setParameter("userToken", userToken);
+        query.setParameter("gameToken", gameToken);
+
+        return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
+    }
+
+    @Override
+    public List<Match> findMatchesByGameTokenAndUserToken(String gameToken, String userToken, PageRequest pageRequest) {
+        logger.info("Finding matches by UserToken: %s and GameToken: %s".formatted(userToken, gameToken));
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<MatchEntity> cq = cb.createQuery(MatchEntity.class);
+
+        Root<MatchEntity> matchEntityRoot = cq.from(MatchEntity.class);
+        Join<MatchEntity, UserEntity> userJoin = matchEntityRoot.join("users");
+        List<Predicate> predicates = new ArrayList<>();
+
+        predicates.add(cb.equal(userJoin.get("token"), userToken));
+        predicates.add(cb.equal(matchEntityRoot.get("game").get("token"), gameToken));
+
+        cq.where(predicates.toArray(new Predicate[0]));
+
+        System.out.println("PageRequest: " + pageRequest);
+
+        TypedQuery<MatchEntity> query = em.createQuery(cq);
+        query.setFirstResult((int) pageRequest.page() * pageRequest.size());
+        query.setMaxResults(pageRequest.size());
+
+
+        return query.getResultList().stream().map(matchMapper::matchEntityToMatchModel).toList();
+
+    }
+
+    @Override
+    public Boolean existsMatchById(Long matchId) {
+        return findMatchById(matchId).isPresent();
+    }
 }
--- gamertrack-db/src/test/java/com/gepardec/adapter/output/persistence/repository/GamertrackDbIT.java
+++ gamertrack-db/src/test/java/com/gepardec/adapter/output/persistence/repository/GamertrackDbIT.java
@@ -7,52 +7,55 @@ import com.gepardec.core.repository.ScoreRepository;
 import com.gepardec.core.services.TokenService;
 import com.gepardec.impl.service.TokenServiceImpl;
 import com.gepardec.model.Score;
+import jakarta.data.page.PageRequest;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
 import jakarta.transaction.UserTransaction;
-import java.util.Arrays;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
+import java.util.Arrays;
+
 public class GamertrackDbIT {
 
-  @PersistenceContext
-  private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-  @Inject
-  private UserTransaction utx;
+    @Inject
+    private UserTransaction utx;
 
-  void removeTableData(Class<?>... tClasses) throws Exception {
-    utx.begin();
-    em.joinTransaction();
-    Arrays.stream(tClasses)
-        .map(this::composeDeleteQuery)
-        .forEach(Query::executeUpdate);
-    utx.commit();
-  }
+    void removeTableData(Class<?>... tClasses) throws Exception {
+        utx.begin();
+        em.joinTransaction();
+        Arrays.stream(tClasses)
+                .map(this::composeDeleteQuery)
+                .forEach(Query::executeUpdate);
+        utx.commit();
+    }
 
-  Query composeDeleteQuery(Class<?> tClass) {
-    return em.createQuery("delete from %s".formatted(tClass.getSimpleName()));
-  }
+    Query composeDeleteQuery(Class<?> tClass) {
+        return em.createQuery("delete from %s".formatted(tClass.getSimpleName()));
+    }
 
-  @Deployment
-  public static Archive<?> createDeployment() {
-    return ShrinkWrap.create(JavaArchive.class, "test.jar")
-        .addClasses(EntityManager.class)
-        .addPackage(ScoreEntity.class.getPackage())
-        .addPackage(ScoreRepository.class.getPackage())
-        .addPackage(ScoreRepositoryImpl.class.getPackage())
-        .addPackage(Score.class.getPackage())
-        .addPackage(MatchMapper.class.getPackage())
-        .addPackage(TestFixtures.class.getPackage())
-        .addClass(TokenService.class)
-        .addClass(TokenServiceImpl.class)
-        .addAsManifestResource("beans.xml")
-        .addAsManifestResource("persistence.xml");
-  }
+    @Deployment
+    public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+                .addClasses(EntityManager.class)
+                .addPackage(ScoreEntity.class.getPackage())
+                .addPackage(ScoreRepository.class.getPackage())
+                .addPackage(ScoreRepositoryImpl.class.getPackage())
+                .addPackage(Score.class.getPackage())
+                .addPackage(MatchMapper.class.getPackage())
+                .addPackage(TestFixtures.class.getPackage())
+                .addClass(TokenService.class)
+                .addClass(TokenServiceImpl.class)
+                .addClass(PageRequest.class)
+                .addAsManifestResource("beans.xml")
+                .addAsManifestResource("persistence.xml");
+    }
 
 }
