a1a28e4095b4933f784b383c6bd15f84710171f3
jaroslav380 <jaroslav.romashchuk@gepardec.com>
2025-02-12 14:38:37 +0100

    add integration test for AuthResource issue #10

--- gamertrack-IntegrationTest/src/test/java/com/gepardec/rest/impl/AuthResourceImplIT.java
+++ gamertrack-IntegrationTest/src/test/java/com/gepardec/rest/impl/AuthResourceImplIT.java
@@ -1,32 +1,96 @@
 package com.gepardec.rest.impl;
 
+import com.gepardec.rest.model.command.AuthCredentialCommand;
+import com.gepardec.rest.model.command.CreateUserCommand;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.Matchers.equalTo;
+
 public class AuthResourceImplIT {
-  /*
+    static List<String> usedUserTokens = new ArrayList<>();
+    String bearerToken;
+
+    static Dotenv dotenv = Dotenv.configure().directory("../").filename("secret.env").ignoreIfMissing().load();
+    private static final String SECRET_DEFAULT_PW = dotenv.get("SECRET_DEFAULT_PW", System.getenv("SECRET_DEFAULT_PW"));
+
     @BeforeAll
     public static void setup() {
-        reset();
-        port = 8080;
-        basePath = "gepardec-gamertrack/api/v1/auth";
+        RestAssured.baseURI = "http://localhost:8080/gepardec-gamertrack/api/v1";
         enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL);
     }
 
-    @Test
-    void ensureLogin() {
-        //GIVEN
-        GameRestDto existingGame = createGame();
-        //WHEN THEN
-        var foundGames =
-                when()
-                        .get()
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .body()
-                        .jsonPath()
-                        .getList("", GameRestDto.class);
-
-        assertTrue(foundGames.contains(existingGame));
+    @AfterEach
+    public void tearDown() {
+        for (String token : usedUserTokens) {
+            with()
+                    .headers(
+                            "Authorization",
+                            "Bearer " + bearerToken,
+                            "Content-Type",
+                            ContentType.JSON,
+                            "Accept",
+                            ContentType.JSON)
+                    .when()
+                    .contentType("application/json")
+                    .pathParam("token", token)
+                    .request("DELETE", "/users/{token}")
+            ;
+        }
     }
 
-   */
+    @Test
+    public void createTestUserWithoutAuthHeader() {
+        with().when()
+                .contentType("application/json")
+                .body(new CreateUserCommand("max","Muster"))
+                .request("POST", "/users")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    public void createTestUserWithAuthHeader() {
+        String authHeader = with().when()
+                .contentType("application/json")
+                .body(new AuthCredentialCommand("admin",SECRET_DEFAULT_PW))
+                .headers("Content-Type", ContentType.JSON,
+                        "Accept", ContentType.JSON)
+                .request("POST", "/auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .header("Authorization");
+
+        bearerToken = authHeader.replace("Bearer ", "");
+
+        String token = with().when()
+                .contentType("application/json")
+                .body(new CreateUserCommand("max","Muster"))
+                .headers(
+                        "Authorization",
+                        "Bearer " + bearerToken,
+                        "Content-Type",
+                        ContentType.JSON,
+                        "Accept",
+                        ContentType.JSON)
+                .request("POST", "/users")
+                .then()
+                .statusCode(201)
+                .assertThat()
+                .body("firstname", equalTo("max"))
+                .extract()
+                .path("token");
+        usedUserTokens.add(token);
+    }
 }
