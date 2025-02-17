89789a82716413a8dc84b21bd95c0db5d1f3deac
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-02-17 15:58:56 +0100

    add exceptions and exceptionHandler
    GameService now throws exceptions and exceptionHandler returns the proper response instead of resource method
    update tests to recent changes
    add ErrorMessage.class to project
    
    issue: #33

--- gamertrack-domain/src/main/java/com/gepardec/impl/service/GameServiceImpl.java
+++ gamertrack-domain/src/main/java/com/gepardec/impl/service/GameServiceImpl.java
@@ -2,6 +2,8 @@ package com.gepardec.impl.service;
 
 import com.gepardec.core.repository.GameRepository;
 import com.gepardec.core.services.*;
+import com.gepardec.foundation.exception.ApplicationException;
+import com.gepardec.foundation.exception.ErrorMessage;
 import com.gepardec.model.Game;
 import com.gepardec.model.Match;
 import com.gepardec.model.Score;
@@ -21,99 +23,107 @@ import java.util.stream.Collectors;
 @Transactional
 public class GameServiceImpl implements GameService, Serializable {
 
-  private final Logger logger = LoggerFactory.getLogger(GameServiceImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(GameServiceImpl.class);
 
-  @Inject
-  private GameRepository gameRepository;
-  @Inject
-  TokenService tokenService;
-  @Inject
-  private UserService userService;
-  @Inject
-  private ScoreService scoreService;
+    @Inject
+    private GameRepository gameRepository;
+    @Inject
+    TokenService tokenService;
+    @Inject
+    private UserService userService;
+    @Inject
+    private ScoreService scoreService;
 
-  @Inject
-  private MatchService matchService;
+    @Inject
+    private MatchService matchService;
 
-  @Override
-  public Optional<Game> saveGame(Game game) {
-    logger.info("Saving game: %s".formatted(game));
+    @Override
+    public Optional<Game> saveGame(Game game) {
+        logger.info("Saving game: %s".formatted(game));
 
-    if (gameRepository.gameExistsByGameName(game.getName())) {
-      return Optional.empty();
-    }
-    game.setToken(tokenService.generateToken());
-    Optional<Game> savedGame = gameRepository.saveGame(game);
+        if (gameRepository.gameExistsByGameName(game.getName())) {
+            String errorMessage = "Game with name %s already exists".formatted(game.getName());
+            logger.error(errorMessage);
+            throw new ApplicationException.GameAlreadyExistsException(new
+                    ErrorMessage(errorMessage,
+                    errorMessage + "\n Duplicate game not allowed. Choose another name or use the already existing game"));
+        }
+        game.setToken(tokenService.generateToken());
+        Optional<Game> savedGame = gameRepository.saveGame(game);
 
-    List<User> userList = userService.findAllUsers(true);
-    if (!userList.isEmpty()) {
+        List<User> userList = userService.findAllUsers(true);
+        if (!userList.isEmpty()) {
 
-      for (User user : userList) {
-        scoreService.saveScore(new Score(0L, user, savedGame.get(), 1500L, "",true));
-      }
-    }
+            for (User user : userList) {
+                scoreService.saveScore(new Score(0L, user, savedGame.get(), 1500L, "", true));
+            }
+        }
 
-    return savedGame;
-  }
-
-  @Override
-  public Optional<Game> deleteGame(String token) {
-    logger.info("Deleting game with token %s".formatted(token));
-    Optional<Game> game = gameRepository.findGameByToken(token);
-    if (game.isEmpty()) {
-      return Optional.empty();
+        return savedGame;
     }
 
-    List<Match> matchesByGame = matchService.findAllMatches().stream().filter(score -> score.getGame().getToken().equals(token)).collect(Collectors.toList());
+    @Override
+    public Optional<Game> deleteGame(String token) {
+        logger.info("Deleting game with token %s".formatted(token));
+        Optional<Game> game = gameRepository.findGameByToken(token);
+        if (game.isEmpty()) {
+            return Optional.empty();
+        }
 
-    List<Score> scoresByGame = scoreService.filterScores(null,null,null,token,true);
+        List<Match> matchesByGame = matchService.findAllMatches().stream().filter(score -> score.getGame().getToken().equals(token)).collect(Collectors.toList());
 
-    for (Match match : matchesByGame) {
-      matchService.deleteMatch(match.getToken());
+        List<Score> scoresByGame = scoreService.filterScores(null, null, null, token, true);
+
+        for (Match match : matchesByGame) {
+            matchService.deleteMatch(match.getToken());
+        }
+
+        for (Score score : scoresByGame) {
+            scoreService.deleteScore(score.getToken());
+        }
+
+        gameRepository.deleteGame(game.get().getId());
+
+        return game;
     }
 
-    for (Score score : scoresByGame) {
-      scoreService.deleteScore(score.getToken());
+    @Override
+    public Optional<Game> updateGame(Game game) {
+        Optional<Game> newGame = Optional.empty();
+
+        if (game == null) {
+            logger.error("Game is null");
+            return Optional.empty();
+        }
+
+        logger.info("Updating game with token %s".formatted(game.getToken()));
+        Optional<Game> gameOld = findGameByToken(game.getToken());
+
+        if (gameOld.isPresent()) {
+            game.setId(gameOld.get().getId());
+            logger.info("Updating game with new values %s".formatted(game));
+            newGame = gameRepository.updateGame(game);
+            return newGame;
+        }
+
+        String errorMessage = "Game with token %s could not be found".formatted(game.getToken());
+
+        logger.error(errorMessage);
+        throw new ApplicationException.GameDoesNotExistException(
+                new ErrorMessage(errorMessage, errorMessage +
+                        "\n Game not found or might have been removed already. Please check the token and try again"));
     }
 
-    gameRepository.deleteGame(game.get().getId());
-
-    return game;
-  }
-
-  @Override
-  public Optional<Game> updateGame(Game game) {
-    Optional<Game> newGame = Optional.empty();
-
-    if (game == null) {
-      logger.error("Game is null");
-      return Optional.empty();
+    @Override
+    public Optional<Game> findGameByToken(String token) {
+        logger.info("Finding game with token %s".formatted(token));
+        return gameRepository.findGameByToken(token);
     }
 
-    logger.info("Updating game with token %s".formatted(game.getToken()));
-    Optional<Game> gameOld = findGameByToken(game.getToken());
-
-    if (gameOld.isPresent()) {
-      game.setId(gameOld.get().getId());
-      logger.info("Updating game with new values %s".formatted(game));
-      newGame = gameRepository.updateGame(game);
+    @Override
+    public List<Game> findAllGames() {
+        logger.info("Finding all games");
+        return gameRepository.findAllGames();
     }
-
-    logger.error("Game with token %s could not be found".formatted(game.getToken()));
-    return newGame;
-
-  }
-
-  @Override
-  public Optional<Game> findGameByToken(String token) {
-    logger.info("Finding game with token %s".formatted(token));
-    return gameRepository.findGameByToken(token);
-  }
-
-  @Override
-  public List<Game> findAllGames() {
-    logger.info("Finding all games");
-    return gameRepository.findAllGames();
-  }
 }
 
