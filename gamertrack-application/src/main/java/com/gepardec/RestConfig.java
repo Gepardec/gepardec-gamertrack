25fe6fd52effeabad49db4cd4b233ea42890634f
Aleksandar Zivkovic <aleksandar.zivkovic@gepardec.com>
2025-08-05 16:30:51 +0200

    refactor: add failsafe plugin to parent pom,
    remove unused RestConfig(not required in Quarkus)
    
    add manually application.properties for tests since kai does not do it

--- gamertrack-application/src/main/java/com/gepardec/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gepardec;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("api/v1")
-public class RestConfig extends Application {
-}
